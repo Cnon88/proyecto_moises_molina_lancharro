87b5bf191d90e56b3663ef0bb1c593eec92f4051
Moisés Molina <cnon@hotmail.es>
2025-06-07 17:33:10 +0200

    Habilitar Cors para internet, etc

--- backend/src/main/java/quedadas/moisesmolinalancharro/backend/config/OpenApiConfig.java
+++ backend/src/main/java/quedadas/moisesmolinalancharro/backend/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,6 +18,10 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://actively-glad-roughy.ngrok-free.app");
+
         return new OpenAPI()
                 .info(new Info().title("Quedadas Games API con JWT").version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
@@ -26,6 +33,7 @@ public class OpenApiConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
                         )
-                );
+                )
+                .servers(List.of(httpsServer));
     }
 }
